cacc7cb9a812adda9017ca157dc67a882b908f51
Arekkasu <alexanderlozada230@gmail.com>
2023-09-16 22:39:30 -0500

    PreAlpha formulario

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("RUNNING");
 
 function Imprimiemail(){
 
+
+    /*
+    *SE SELECCIONAN DIRECTAMENTE EN LA FUCION LOS VALORES DE LOS INPUTS, YA QUE SI SE INICIALIZA ANTES ESTE VALOR
+    *SERIA NULO Y DARIA ERRORES AL USARLO EN LA FUNCION DONDE EL IF NO FUNCIONARIA Y DARIA ERROR DE TIPADO
+    * 
+    */
     let email = document.querySelector("#email-adress");
     let advertencia = document.querySelector("#warning");
     let text_area = document.querySelector("#text-area");
